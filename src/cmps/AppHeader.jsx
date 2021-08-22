e64fb5461e108109ee6d2becd2a68b89a8df4f36
Yaniv bar <yanivb527@gmail.com>
2021-08-22 12:42:27 +0300

    sunday commit- Bass scss and nave header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -6,21 +6,28 @@ export default function AppHeader(){
 
   return(  
     <header className="main-header">
+      <div className="header-container">
      <p>Natural 1</p>
      <ul>
        <li>
-       <NavLink activeClassName="active-nav" exact to="/">Home | </NavLink>
+       <NavLink activeClassName="active-nav" exact to="/">Home</NavLink>
 
        </li>
    
        <li>
-       <NavLink activeClassName="active-nav" exact to="/charactermanager">Characters | </NavLink>
+       <NavLink activeClassName="active-nav" exact to="/charactermanager">Characters</NavLink>
        </li>
 
        <li>
-       <NavLink activeClassName="active-nav" exact to="/games">games | </NavLink>
+       <NavLink activeClassName="active-nav" exact to="/games">games</NavLink>
+       </li>
+
+       
+       <li>
+       <NavLink activeClassName="active-nav" exact to="/about">about</NavLink>
        </li>
      </ul>
+     </div>
 
     </header>
   )
